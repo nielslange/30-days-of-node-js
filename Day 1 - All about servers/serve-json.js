4fba2199fef91b277e2f0b3b85bb1685c766a937
Niels Lange <info@nielslange.de>
2018-11-12 20:43:28 +0800

    Add serve-json.js

---
+++ Day 1 - All about servers/serve-json.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const http = require('http');
+const host = '127.0.0.1';
+const port = 3000;
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+    console.log('Server started');
+
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    const data = {
+        status : 200,
+        message : 'sucessful',
+        result : [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ],
+        code : 2000
+    }
+    response.write(JSON.stringify(data));
+    response.end();
+
+    console.log('Server responded');
+});
+
+server.listen(port, host, (error) => {
+    if (error) {
+        throw Error(`Error: ${error}`);
+    }
+
+    console.log(`Server is listening on ${host}:${port}`);
+});
