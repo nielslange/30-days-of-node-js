4a8100fd393206dd9be36ab6f70e2fb311a18003
Niels Lange <info@nielslange.de>
2018-11-12 20:04:31 +0800

    Add serve-html.js

---
+++ Day 1 - All about servers/serve-html.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const http = require('http');
+const host = '127.0.0.1';
+const port = 3000;
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+    console.log('Server started');
+
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', (error, data) => {
+        if (error) {
+            throw Error(`Error: ${error}`);
+        }
+        response.end(data);
+    });
+    
+    console.log('Server responded');
+});
+
+server.listen(port,host, (error) => {  
+    if (error) {
+        throw Error(`Error: ${error}`);
+    }
+  
+    console.log(`Server is listening on ${host}:${port}`);
+  });	
