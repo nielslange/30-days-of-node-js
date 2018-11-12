0fb2479e337d519edc8c686cfd79f6966353fe3f
Niels Lange <info@nielslange.de>
2018-11-12 23:31:41 +0800

    Add serve-mp4.js

---
+++ Day 1 - All about servers/serve-mp4.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const http  = require('http');
+const host  = '127.0.0.1';
+const port  = 3000;
+const fs    = require('fs');
+const file  = 'demo.mp4';
+
+const server = http.createServer((request, response) => {
+    console.log('Server started');
+
+    fs.exists(file, (data) => {
+		if (data) {
+			response.writeHead(200, {'Content-Type': 'video/mp4'});
+            const stream = fs.createReadStream(file);
+			stream.pipe(response);
+		} else {
+			response.writeHead(404, {'Content-Type': 'text/plain'});
+            response.write('Error 404: Video file not found!');
+            response.end();
+		}
+	});
+});
+
+server.listen(port, host, (error) => {
+    if (error) {
+        throw error;
+    }
+
+    console.log(`Server is listening on ${host}:${port}`);
+});
