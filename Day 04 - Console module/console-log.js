6ec64ba42e9ff9710c3e8d150bdffd1237850e25
Niels Lange <info@nielslange.de>
2018-11-15 11:33:54 +0800

    Create console-log.js

---
+++ Day 04 - Console module/console-log.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const string = 'Node.js';
+const boolean = true;
+const integer = 23;
+const float = 2.3;
+
+console.log('1: Output surrounded by \'...\'');
+console.log("2: Output surrounded by \"...\"");
+console.log(`3: Output of the string "${string}"`);
+console.log(`4: Output of the boolean "${boolean}"`);
+console.log(`5: Output of the integer "${integer}"`);
+console.log(`6: Output of the float "${float}"`);
