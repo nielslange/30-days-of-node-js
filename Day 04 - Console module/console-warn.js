69ba19939ead78b8c1b9112025889b6d2b024c41
Niels Lange <info@nielslange.de>
2018-11-15 11:34:10 +0800

    Create console-warn.js

---
+++ Day 04 - Console module/console-warn.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let x = 2;
+let y = 3;
+let result = x/y;
+
+if ( result ==  1.5 ) {
+    console.log(`✅ Result: ${result}`);
+} else {
+    console.warn('❕ Warning: Operands wrong positioned!');
+}
