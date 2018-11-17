db59d7f13d945a85555b235a08e0a7b02c66e0f7
Niels Lange <info@nielslange.de>
2018-11-17 23:49:43 +0800

    Create array-push.js

---
+++ Day 06 - Array methods/array-push.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let seasons = new Array('Spring', 'Summer');
+console.log(seasons);
+
+seasons.push('Autum');
+console.log(seasons);
+
+seasons.push('Winter');
+console.log(seasons);
