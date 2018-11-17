50b3212dc38302be7cc09bba17302dcd2f0197ea
Niels Lange <info@nielslange.de>
2018-11-17 23:51:27 +0800

    Create array-concat.js

---
+++ Day 06 - Array methods/array-concat.js
@@ -0,0 +1,9 @@
+'use strtict';
+
+let vegetables = ['tomatoes', 'onions', 'peppers'];
+let fruits = ['strawberries, cherries'];
+console.log(vegetables);
+console.log(fruits);
+
+let market = vegetables.concat(fruits);
+console.log(market);
