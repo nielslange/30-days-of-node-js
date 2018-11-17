42a72a2aac0882a2a8dfed933b4497f27de2c2e5
Niels Lange <info@nielslange.de>
2018-11-17 23:49:26 +0800

    Create array-create.js

---
+++ Day 06 - Array methods/array-create.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let names = ['Devin', 'Niels', 'Liki', 'Kilang', 'Flexo'];
+console.log(names);
+
+let length = names.length;
+console.log(length);
+
+let fibonacci = new Array(10);
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+console.log(fibonacci);
+
+let lucas = new Array(2, 1, 3, 4, 7, 11, 18, 29, 47, 76);
+console.log(lucas);
