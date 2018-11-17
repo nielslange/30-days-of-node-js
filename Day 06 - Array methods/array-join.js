34295847b21bc444f04706bd82d7e8de62c437cd
Niels Lange <info@nielslange.de>
2018-11-17 23:52:02 +0800

    Create array-join.js

---
+++ Day 06 - Array methods/array-join.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let numbers = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 ];
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+console.log(numbers);
+console.log(letters);
+
+let numbersArray = numbers.join();
+let lettersArray = letters.join(':');
+
+console.log(numbersArray);
+console.log(lettersArray);
