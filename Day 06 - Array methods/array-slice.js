6ef1c7f3d8189d7c1b844b0f8c9c12e0efebb0ff
Niels Lange <info@nielslange.de>
2018-11-17 23:51:15 +0800

    Create array-slice.js

---
+++ Day 06 - Array methods/array-slice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let numbers = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 ];
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+console.log(numbers);
+console.log(letters);
+
+let numbersArray = numbers.slice(2, 4);
+let lettersArray = letters.slice(-2, -1);
+
+console.log(numbersArray);
+console.log(lettersArray);
