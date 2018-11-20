935c9a4fdb2e624edf4ef73f735ae47c4d3bc346
Niels Lange <info@nielslange.de>
2018-11-20 17:10:31 +0700

    Create crypto-create-hmac-sha512.js

---
+++ Day 09 - Hashing and HMAC/crypto-create-hmac-sha512.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const crypto = require('crypto');
+const algorithm = 'sha512';
+const password = 'QhjPtYE#fogWa=3jnEmjM8og4kWymiTN[=Yk[66CT8M;6);XQC,nX3Fjj8Dh}7QX';
+const message = '30 Days of Node.js';
+const hmac = crypto.createHmac(algorithm, password);
+const data = hmac.update(message);
+const gen_hmac = data.digest('hex');
+
+console.log(`ℹ️ HMAC: ${gen_hmac}`);
