9db313d09e25a8310567fc2817a5bd0bb1849b8e
Niels Lange <info@nielslange.de>
2018-11-20 17:09:44 +0700

    Create crypto-create-hash-sha256.js

---
+++ Day 09 - Hashing and HMAC/crypto-create-hash-sha256.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const crypto = require('crypto');
+const algorithm = 'sha256';
+const message = '30 Days of Node.js';
+const inputEncoding = 'utf-8';
+const outputEncoding = 'hex';
+const hash = crypto.createHash(algorithm);
+const data = hash.update(message, inputEncoding);
+const gen_hash = data.digest(outputEncoding);
+
+console.log(`ℹ️ HASH: ${gen_hash}`);
