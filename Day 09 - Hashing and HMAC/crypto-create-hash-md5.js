146f1ba96e50a874a165b0bc270d4df910d110b1
Niels Lange <info@nielslange.de>
2018-11-20 17:08:13 +0700

    Create crypto-create-hash-md5.js

---
+++ Day 09 - Hashing and HMAC/crypto-create-hash-md5.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const crypto = require('crypto');
+const algorithm = 'md5';
+const message = '30 Days of Node.js';
+const encoding = 'hex';
+const hash = crypto.createHash(algorithm);
+const data = hash.update(message);
+const gen_hash = data.digest(encoding);
+
+console.log(`ℹ️ HASH: ${gen_hash}`);
