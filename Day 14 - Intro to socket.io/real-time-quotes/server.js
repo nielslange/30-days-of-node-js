875a3a0d6f39bbd9eb02eaf41a691b993df425c2
Niels Lange <info@nielslange.de>
2018-11-25 15:50:53 +0800

    Create real-time-quotes/server.js

---
+++ Day 14 - Intro to socket.io/real-time-quotes/server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(3000);
+console.log('ℹ️  Server listening on port 3000');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/admin' , (req,res) => {
+    res.sendFile(__dirname + '/admin.html'); 
+})
+
+io.on('connection', (socket) => {
+    socket.emit('welcome', { data: 'welcome' });
+    socket.on('new', (data) => {   
+        console.log(`✅  Quote "${data.quote}" addedd successfully!`);
+        io.sockets.emit('next', { data : data });
+    });
+});
